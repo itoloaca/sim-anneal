ac43685ee26794434dc1307b526a7781c60bf4ad
Ion Toloaca <(none)>
2016-04-09 11:17:15 +0200

    Revert proposal and logging

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from colormath.color_objects import LabColor, sRGBColor
 from colormath.color_conversions import convert_color
 from scipy.misc import toimage
 from scipy import ndimage
-from scipy import stats
 import cv2
 
 
@@ -28,26 +27,26 @@ class Data:
 		self.width = width
 		self.energyCol = np.zeros(self.width - 1)
 		self.energyRow = np.zeros(self.height - 1)
-	
+
 	# 0 <= j < width - 1
 	def computeEnergyCol(self, j):
 		diff = self.image[:, 25 * (j + 1) - 1, :] - self.image[:, 25 * (j + 1), :]
 		return np.linalg.norm(diff)
 		
+
 	# 0 <= i < height - 1
 	def computeEnergyRow(self, i):
 		diff = self.image[25 * (i + 1) - 1, :, :] - self.image[25 * (i + 1), :, :]
 		return np.linalg.norm(diff)
-
+		
 
 	def energy(self):
 		for j in range(self.width - 1):
 			self.energyCol[j] = self.computeEnergyCol(j)
 		for i in range(self.height - 1):
-			self.energyRow[i] = self.computeEnergyRow(i) 
-		return self.energyRow.sum() + self.energyCol.sum() 
+			self.energyRow[i] = self.computeEnergyRow(i)
+		return self.energyRow.sum() + self.energyCol.sum()
 
-	
 	def show(self):
 		toimage(self.image).show()
 
@@ -58,18 +57,11 @@ class Data:
 		self.image[25*i2 : 25*(i2+1), 25*j2 : 25*(j2+1), :] = t1
 
 	def proposal(self):
-		data1 = self.energyRow / self.energyRow.sum()
-		data2 = self.energyCol / self.energyCol.sum()
-
-		pdf1 = stats.rv_discrete(values=(np.arange(self.height - 1), data1))
-		pdf2 = stats.rv_discrete(values=(np.arange(self.width - 1), data2))
-	
-		x1, x2 = np.clip(pdf1.rvs(size = 2), 1, self.height - 1)
-		y1, y2 = np.clip(pdf2.rvs(size = 2), 1, self.width - 1)
+		x1 = np.random.randint(1, self.height - 1)
+		y1 = np.random.randint(1, self.width - 1)
+		x2 = np.random.randint(1, self.height - 1)
+		y2 = np.random.randint(1, self.width - 1)
 		return [(x1, y1), (x2, y2)]
-
-	def cheatEnergy(self):
-		return np.linalg.norm(self.image - original)
 
 class Anneal:
 	def __init__(self, data):
@@ -77,39 +69,21 @@ class Anneal:
 		self.energy = data.energy()
 		self.bestEnergy = self.energy
 
-		self.pAcceptArr = np.array([])
-		self.acceptedArr = np.array([])
-		self.rejectedArr = np.array([])
-		self.tempArr = np.array([])
-		self.energyArr = np.array([])
-		self.cheatEnergyArr = np.array([])
-		self.pAcceptCurr =  0.0
-		self.rejCurr = 0.0
-		self.acceptedCurr = 0.0
-		self.energyCurr = 0.0
-
-
-	def step(self, temperature, i):
+	def step(self, temperature):
 		prop = self.data.proposal()
 		x1, y1 = prop[0]
 		x2, y2 = prop[1]
 		self.data.swap(x1, y1, x2, y2)
 		newEnergy = self.data.energy()
 		delta = newEnergy - self.energy
-		self.energyCurr += newEnergy
 		if delta <= 0:
 			self.energy = newEnergy
-			self.acceptedCurr += 1
-			self.pAcceptCurr += 1
 		else:
 			pAccept = np.exp(- delta / temperature)
-			self.pAcceptCurr += pAccept
 			if (np.random.rand() <= pAccept):
 				self.energy = newEnergy
-				self.acceptedCurr += 1
 			else:
 				self.data.swap(x1, y1, x2, y2)
-				self.rejCurr += 1
 
 
 	def do(self, startExp, endExp, numStepsExp):
@@ -120,47 +94,15 @@ class Anneal:
 			if (self.energy < self.bestEnergy):
 				self.bestEnergy = self.energy
 				print("E=" + str(self.energy) + " at t=" + str(temperature))
+			self.step(temperature)
 			i += 1
-			self.step(temperature, i)
-			rate = 10**3
-			if (i % rate == 0):
-				self.tempArr = np.append(self.tempArr, temperature)
-				self.energyArr = np.append(self.energyArr, self.energyCurr / rate)
-				self.pAcceptArr = np.append(self.pAcceptArr, self.pAcceptCurr / rate)
-				self.acceptedArr = np.append(self.acceptedArr, self.acceptedArr / rate)
-				self.rejectedArr = np.append(self.rejectedArr, self.rejCurr / rate)
-				self.cheatEnergyArr = np.append(self.cheatEnergyArr, self.data.cheatEnergy())
-				self.energyCurr = 0.0
-				self.pAcceptCurr = 0.0
-				self.acceptedCurr = 0.0
-				self.rejCurr = 0.0
-
-	def logs(self):
-		f, axarr = plt.subplots(7)
-		axarr[0].plot(self.tempArr, 'b^')
-		axarr[0].set_title('tempArr')
-		axarr[1].plot(self.energyArr, 'b^')
-		axarr[1].set_title('energyArr')
-		axarr[2].plot(self.rejectedArr, 'b^')
-		axarr[2].set_title('rejectedArr')
-		axarr[3].plot(self.pAcceptArr, 'b^')
-		axarr[3].set_title('pAcceptArr')
-		axarr[4].semilogy(self.tempArr, 'b^')
-		axarr[4].set_title('tempArr')
-		axarr[5].plot(self.energyArr, 'b^')
-		axarr[5].set_title('energyArr')
-		axarr[6].plot(self.cheatEnergyArr, 'r^')
-		axarr[6].set_title('cheatEnergyArr')
-		plt.show()
-
-
 
 d = Data(shuffledImage)
 a = Anneal(d)
-# a.do(2.7, 0, 6)
-# a.data.show()
-dOrg = Data(original)
-aOrg = Anneal(dOrg)
+a.do(startExp = 3, endExp = 0, numStepsExp = 5) # I tried all kinds of cooling schemes (while also logging all possible params) 
+a.data.show()
+dOriginal = Data(original)
+aOriginal = Anneal(dOriginal)
 
 
 
